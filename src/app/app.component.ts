c6e7ef8d3b4d25402f60297c301f1558915604c7
fayebuchanan <faye.buchanan@crm-uk.com>
2024-02-07 18:45:37 +0000

    feat: Added styling to main page, as well as a mock table
    I will turn the table into a component that automatically builds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TableComponent } from './Components/table/table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TechTest';
+  title = 'Elcom Tech Test';
 }
