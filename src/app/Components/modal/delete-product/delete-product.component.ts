5b6fa8abba84770d6c633b35714f8eba41b94de2
Faye Buchanan <zirilyth@gmail.com>
2024-02-09 21:34:21 +0000

    feat: Added delete functionality

--- src/app/Components/modal/delete-product/delete-product.component.ts
+++ src/app/Components/modal/delete-product/delete-product.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, Output, TemplateRef } from '@angular/core';
+import { BackendService, Product } from '../../../Services/backend.service';
+import { Subject } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-delete-product',
 	standalone: true,
-	imports: [],
+	imports: [
+		FormsModule,
+		ReactiveFormsModule
+	],
 	templateUrl: './delete-product.component.html',
 	styleUrl: './delete-product.component.css'
 })
 export class DeleteProductComponent {
+	@Input() productID: number;
 
+	@Output() productIDChange = new Subject<number>();
+
+	private modalService = inject(NgbModal);
+
+	constructor(
+		private backendService: BackendService
+	) {
+		this.productID = 0
+	}
+
+	open(content: TemplateRef<any>) {
+		this.modalService.open(content, {ariaLabelledBy: 'edit-product-modal'});
+
+	}
+
+	close() {
+		this.modalService.dismissAll();
+	}
+
+	delete() {
+		this.backendService.deleteProduct(this.productID).subscribe((response) => {
+			this.productIDChange.next(this.productID)
+			this.modalService.dismissAll()
+		});
+	}
 }
--- src/app/Components/table/table.module.ts
+++ src/app/Components/table/table.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table.component';
 import { EditProductComponent } from '../modal/edit-product/edit-product.component';
+import { DeleteProductComponent } from '../modal/delete-product/delete-product.component';
 
 
 @NgModule({
 	declarations: [TableComponent],
 	imports: [
 		CommonModule,
-		EditProductComponent
+		EditProductComponent,
+		DeleteProductComponent
 	],
 	exports: [TableComponent]
 })
